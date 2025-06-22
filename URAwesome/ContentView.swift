88612513b4581bdab6acb65fa5c81284ff04912a
primalkarma <primalkarma@gmail.com>
2025-06-22 23:47:40 +0530

    Initial Commit

---
+++ URAwesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URAwesome
+//
+//  Created by Parimal Devi on 22/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URAwesome/URAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  URAwesomeApp.swift
+//  URAwesome
+//
+//  Created by Parimal Devi on 22/06/25.
+//
+
+import SwiftUI
+
+@main
+struct URAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
